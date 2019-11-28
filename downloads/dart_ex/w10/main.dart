306c4d655f8c33eb80de18921b2f780cf3c05e10
s40523124 <40523124@gm.nfu.edu.tw>
2019-11-28 15:13:13 +0800

    week 12 update

--- downloads/dart_ex/w10/main.dart
+++ downloads/dart_ex/w10/main.dart
@@ -1,12 +1,22 @@
-int Ctemp;
-int Ftemp;
+//定義全域變數，num定義後面的變數代表"浮點數或整數"
+num Celsius;
+num Fahrenheit;
+num C;
+num F;
+
+//主程式開始編譯
 void main() {
-//於此輸入攝氏溫度
-  Ctemp=(25);
-  //印出華氏溫度
-    print(Ctemp*1.8+32);
-//於此輸入華氏溫度
-  Ftemp=(77);
-  //印出攝氏溫度
-    print((Ftemp-32)/1.8);
+    //於此輸入愈轉換之攝氏溫度溫度
+    Celsius=(25);
+    //於此輸入愈轉換之華氏溫度溫度
+    Fahrenheit=(86);
+
+    //將F定義為攝氏溫度轉換後的華氏溫度
+    F=Celsius*1.8+32;
+    //將C定義為華氏溫度轉換後的攝氏溫度
+    C=(Fahrenheit-32)/1.8;
+
+        //印出溫度轉換後之結果
+        print("若攝氏溫度=$Celsius°C時，華氏溫度=$F°F。");
+        print("若華氏溫度=$Fahrenheit°F時，攝氏溫度=$C°C。");
 }
